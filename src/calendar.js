c39b2d8bc3adb7bbdb2e681f5fee8c9a93b26065
Peter <peter.mihalik@gmail.com>
2017-08-18 17:20:32 +0200

    feat(padLeft): create padLeft filter

--- src/calendar.js
+++ src/calendar.js
@@ -3,6 +3,21 @@
 
     angular.module('calendar', []);
         
+    angular.module('calendar').filter('padLeft', [
+        function() {
+            return function(val, noOfChars, char) {
+                if (val) {
+                    val = val.toString();
+                    while(val.length < noOfChars) {
+                        val = char + val;
+                    }
+                }
+                
+                return val;
+            }
+        }
+    ]);
+
     angular.module('calendar').constant('calendarConstants', Object.freeze({
         DAY: 1,
         WEEK: 2
