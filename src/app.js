5287358667a04c19310b31a1fdf8ef46f2ab0905
Peter <peter.mihalik@gmail.com>
2017-08-20 16:47:44 +0200

    feat(app): add more dates to sample

--- src/app.js
+++ src/app.js
@@ -9,8 +9,11 @@
                 selectedDate: new Date(2017, 7, 18),
                 mode: calendarConstants.DAY,
                 data: [
+                    {from: new Date(2017, 7, 18, 8, 0, 0), to:new Date(2017, 7, 18, 8, 43, 0), title: "abc" },
                     {from: new Date(2017, 7, 18, 12, 0, 0), to:new Date(2017, 7, 18, 14, 0, 0), title: "foo" },
-                    {from: new Date(2017, 7, 18, 15, 0, 0), to:new Date(2017, 7, 18, 18, 0, 0), title: "bar" }
+                    {from: new Date(2017, 7, 18, 13, 0, 0), to:new Date(2017, 7, 18, 14, 30, 0), title: "collision" },
+                    {from: new Date(2017, 7, 18, 15, 0, 0), to:new Date(2017, 7, 18, 18, 0, 0), title: "bar" },
+                    {from: new Date(2017, 7, 19, 15, 0, 0), to:new Date(2017, 7, 18, 18, 0, 0), title: "xyz" },
                 ]
             };
         }
